1148123d5b15711f6e8346198e92a71555006c61
xiangmu <xiangmu110@qq.com>
2015-02-11 14:33:04 +0800

    work GradientFull

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -3,8 +3,10 @@
 LPCTSTR g_lptszClassName = TEXT("WndClass");
 const int g_nScrWidth = GetSystemMetrics(SM_CXSCREEN);
 const int g_nScrHeight = GetSystemMetrics(SM_CYSCREEN);
-const int g_nTktBorder = 10;
+const int g_nTktBorder = 5;
 const int g_nCapBorder = 20;
+const RECT g_rtInitCliWnd = { 0, 0, 600, 600 };
+const POINT g_ptMinWnd = { 300, 300 };
 
 HWND g_hMainWnd = NULL;
 
@@ -84,7 +86,7 @@ bool InitApplication(
 		return false;
 	}
 
-	RECT rtClient = { 0, 0, 400, 400 };
+	RECT rtClient = g_rtInitCliWnd;
 	DWORD wStyle = WS_POPUP;
 		//WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME;
 	if (! AdjustWindowRect(&rtClient, wStyle, FALSE))
@@ -94,6 +96,7 @@ bool InitApplication(
 
 	int wndWidth = rtClient.right - rtClient.left;
 	int wndHeight = rtClient.bottom - rtClient.top;
+
 	POINT point;
 	point.x = (g_nScrWidth - wndWidth) / 2;
 	point.y = (g_nScrHeight - wndHeight) / 2;
@@ -135,8 +138,8 @@ LRESULT WINAPI OnGetMinMaxInfo_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
 	PMINMAXINFO minmax = NULL;
 	minmax = (PMINMAXINFO)lParam;
-	minmax->ptMinTrackSize.x = 400;
-	minmax->ptMinTrackSize.y = 400;
+	minmax->ptMinTrackSize.x = g_ptMinWnd.x;
+	minmax->ptMinTrackSize.y = g_ptMinWnd.y;
 
 	return 0;
 }
@@ -243,34 +246,80 @@ LRESULT WINAPI OnPaint_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 
 LRESULT WINAPI OnEraseBkgnd_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
+	HDC hdc = (HDC)wParam;
+
 	RECT wndRect;
 	//GetWindowRect(hWnd, &wndRect);//错误，不能获取窗口大小，否则会失败。
 	GetClientRect(hWnd, &wndRect);
 
-	HDC hdc = (HDC)wParam;
+	RECT bufRect = wndRect;
+
 	//边框
-	FillRect(hdc, &wndRect, (HBRUSH)GetStockObject(BLACK_BRUSH));
+	HBRUSH brush = CreateSolidBrush(RGB(0,0,0));
+	FillRect(hdc, &bufRect, brush);
+	DeleteObject(brush);
+
+	//标题栏
+	bufRect.top += g_nTktBorder;
+	bufRect.bottom = g_nCapBorder + g_nTktBorder;
+	bufRect.left += g_nTktBorder;
+	bufRect.right -= g_nTktBorder;
+	FillRect(hdc, &bufRect, (HBRUSH)GetStockObject(GRAY_BRUSH));
+
+	//客户区
+	bufRect.top = g_nCapBorder + g_nTktBorder;
+	bufRect.bottom = wndRect.bottom - g_nTktBorder;
+
+	//FillRect(hdc, &bufRect, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+	TRIVERTEX trives[3];
+	trives[0].x = bufRect.left;
+	trives[0].y = bufRect.top;
+	trives[0].Alpha = 0xff00;
+	trives[0].Red = 0xff00;
+	trives[0].Green = 0 ;
+	trives[0].Blue = 0 ;
+	trives[1].x = bufRect.right ;
+	trives[1].y = bufRect.bottom / 2;
+	trives[1].Alpha = 0xff00;
+	trives[1].Red = 0 ;
+	trives[1].Green = 0xff00;
+	trives[1].Blue = 0;
+	trives[2].x = bufRect.left;
+	trives[2].y = bufRect.bottom;
+	trives[2].Alpha = 0xff00;
+	trives[2].Red = 0;
+	trives[2].Green = 0 ;
+	trives[2].Blue =0xff00 ;
+	// 定义渐变区为矩形，并确定其点
+	GRADIENT_RECT rects[2];
+	rects[0].UpperLeft = 0;
+	rects[0].LowerRight = 1;
+	rects[1].UpperLeft = 1;
+	rects[1].LowerRight = 2;
+	GradientFill(hdc, trives, 3, rects, 2, GRADIENT_FILL_RECT_V);
+
+	/*
+	//创建兼容DC
+	HDC bufHdc = NULL;
+	bufHdc = CreateCompatibleDC(hdc);
+
+	int nRet = 0;
+	//边框
+	nRet = FillRect(bufHdc, &wndRect, (HBRUSH)GetStockObject(BLACK_BRUSH));
 
 	//非边框区
 	wndRect.left += g_nTktBorder;
 	wndRect.top += g_nTktBorder;
 	wndRect.right -= g_nTktBorder;
 	wndRect.bottom -= g_nTktBorder;
-	FillRect(hdc, &wndRect, (HBRUSH)GetStockObject(WHITE_BRUSH));
+	nRet = FillRect(bufHdc, &wndRect, (HBRUSH)GetStockObject(WHITE_BRUSH));
 
 	//标题栏
 	wndRect.bottom = g_nCapBorder + g_nTktBorder;
-	FillRect(hdc, &wndRect, (HBRUSH)GetStockObject(GRAY_BRUSH));
+	nRet = FillRect(bufHdc, &wndRect, (HBRUSH)GetStockObject(GRAY_BRUSH));
 
-	/*
-	HDC bufHdc = CreateCompatibleDC(hdc);
-	FillRect(bufHdc, &wndRect, (HBRUSH)GetStockObject(BLACK_BRUSH));
-	wndRect.left += 10;
-	wndRect.top += 10;
-	wndRect.right -= 10;
-	wndRect.bottom -= 10;
-	FillRect(bufHdc, &wndRect, (HBRUSH)GetStockObject(WHITE_BRUSH));
-	StretchBlt(hdc, wndRect.left, wndRect.top, wndRect.right, wndRect.bottom,
+	nRet = StretchBlt(hdc, wndRect.left, wndRect.top, wndRect.right, wndRect.bottom,
 		bufHdc, wndRect.left, wndRect.top, wndRect.right, wndRect.bottom, SRCCOPY);
 	DeleteDC(bufHdc);
 	*/
