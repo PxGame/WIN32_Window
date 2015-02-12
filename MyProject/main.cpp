3b8891d09b96f83f06d8f4f771bfd648b5d630b1
xiangmu <xiangmu110@qq.com>
2015-02-12 12:20:28 +0800

    work setCursor

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -1,16 +1,18 @@
 # include "main.h"
+# include "resource.h"
 
 LPCTSTR g_lptszClassName = TEXT("WndClass");
 const int g_nScrWidth = GetSystemMetrics(SM_CXSCREEN);
 const int g_nScrHeight = GetSystemMetrics(SM_CYSCREEN);
-const int g_nTktBorder = 5;
+const int g_nTktBorder = 3;
 const int g_nCapBorder = 20;
-const RECT g_rtInitCliWnd = { 0, 0, 600, 600 };
-const POINT g_ptMinWnd = { 300, 300 };
+const int g_nUserBorder = 120;
+const RECT g_rtInitCliWnd = { 0, 0, 280, 700 };
+const POINT g_ptMinWnd = { 280, 500 };
 
+HINSTANCE g_hInstance = NULL;
 HWND g_hMainWnd = NULL;
-
-
+HWND g_btnClose = NULL;
 
 int WINAPI WinMain(
 	HINSTANCE hInstance,
@@ -18,6 +20,8 @@ int WINAPI WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
+	g_hInstance = hInstance;
+
 	BOOL bRet = FALSE;
 	bRet = InitApplication(hInstance);
 	if (bRet == false)
@@ -44,23 +48,28 @@ LRESULT CALLBACK WinMainProc(
 	WPARAM wParam,
 	LPARAM lParam)
 {
-	if (hWnd == g_hMainWnd)
+	switch (uMsg)
 	{
-		switch (uMsg)
-		{
-		case WM_CLOSE:
-			return OnClose_MainWnd(hWnd, wParam, lParam);
-		case WM_DESTROY:
-			return OnDestroy_MainWnd(hWnd, wParam, lParam);
-		case WM_ERASEBKGND:
-			return OnEraseBkgnd_MainWnd(hWnd, wParam, lParam);
-		case WM_GETMINMAXINFO:
-			return OnGetMinMaxInfo_MainWnd(hWnd, wParam, lParam);
-		case WM_LBUTTONDOWN:
-			return OnLbuttonDown_MainWnd(hWnd, wParam, lParam);
-		case WM_PAINT:
-			return OnPaint_MainWnd(hWnd, wParam, lParam);
-		}
+	case WM_CREATE:
+		return OnCreate_MainWnd(hWnd, wParam, lParam);
+	case WM_CLOSE:
+		return OnClose_MainWnd(hWnd, wParam, lParam);
+	case WM_DESTROY:
+		return OnDestroy_MainWnd(hWnd, wParam, lParam);
+	case WM_ERASEBKGND:
+		return OnEraseBkgnd_MainWnd(hWnd, wParam, lParam);
+	case WM_GETMINMAXINFO:
+		return OnGetMinMaxInfo_MainWnd(hWnd, wParam, lParam);
+	case WM_LBUTTONDOWN:
+		return OnLbuttonDown_MainWnd(hWnd, wParam, lParam);
+	case WM_PAINT:
+		return OnPaint_MainWnd(hWnd, wParam, lParam);
+	case WM_SIZE:
+		return OnSize_MainWnd(hWnd, wParam, lParam);
+	case WM_DRAWITEM:
+		return OnDrawItem_MainWnd(hWnd, wParam, lParam);
+	case WM_MOUSEMOVE:
+		return OnMoveMouse_MainWnd(hWnd, wParam, lParam);
 	}
 
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
@@ -72,7 +81,7 @@ bool InitApplication(
 	WNDCLASS wnd;
 	wnd.cbClsExtra = 0;
 	wnd.cbWndExtra = 0;
-	wnd.hbrBackground = CreateSolidBrush(RGB(255, 255, 255));
+	wnd.hbrBackground = NULL;// CreateSolidBrush(RGB(255, 255, 255));
 	wnd.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wnd.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wnd.hInstance = hInstance;
@@ -120,7 +129,6 @@ bool InitApplication(
 	return true;
 }
 
-
 //MainWnd Message
 LRESULT WINAPI OnClose_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
@@ -147,9 +155,10 @@ LRESULT WINAPI OnGetMinMaxInfo_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 LRESULT WINAPI OnLbuttonDown_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
 	
-	POINT point;
-	point.x = LOWORD(lParam);
-	point.y = HIWORD(lParam);
+	POINTS point = { 0 };
+	//point.x = LOWORD(lParam);
+	//point.y = HIWORD(lParam);
+	point = MAKEPOINTS(lParam);
 	
 	RECT wndRect;
 	if (! GetClientRect(hWnd, &wndRect))
@@ -246,6 +255,7 @@ LRESULT WINAPI OnPaint_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 
 LRESULT WINAPI OnEraseBkgnd_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 {
+
 	HDC hdc = (HDC)wParam;
 
 	RECT wndRect;
@@ -253,25 +263,40 @@ LRESULT WINAPI OnEraseBkgnd_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 	GetClientRect(hWnd, &wndRect);
 
 	RECT bufRect = wndRect;
+	HBRUSH brush = NULL;
+	
+	SetBkMode(hdc, TRANSPARENT);
 
 	//边框
-	HBRUSH brush = CreateSolidBrush(RGB(0,0,0));
+	brush = CreateSolidBrush(RGB(102, 204, 255));
 	FillRect(hdc, &bufRect, brush);
 	DeleteObject(brush);
+	
 
 	//标题栏
 	bufRect.top += g_nTktBorder;
 	bufRect.bottom = g_nCapBorder + g_nTktBorder;
 	bufRect.left += g_nTktBorder;
 	bufRect.right -= g_nTktBorder;
-	FillRect(hdc, &bufRect, (HBRUSH)GetStockObject(GRAY_BRUSH));
+	brush = CreateSolidBrush(RGB(102, 204, 255));
+	FillRect(hdc, &bufRect, brush);
+	DeleteObject(brush);
+
+	//用户信息
+	bufRect.top += g_nCapBorder;
+	bufRect.bottom += g_nUserBorder;
+	brush = CreateSolidBrush(RGB(0, 204, 255));
+	FillRect(hdc, &bufRect, brush);
+	DeleteObject(brush);
 
 	//客户区
-	bufRect.top = g_nCapBorder + g_nTktBorder;
+	bufRect.top += g_nUserBorder;
 	bufRect.bottom = wndRect.bottom - g_nTktBorder;
+	brush = CreateSolidBrush(RGB(255, 255, 255));
+	FillRect(hdc, &bufRect, brush);
+	DeleteObject(brush);
 
-	//FillRect(hdc, &bufRect, (HBRUSH)GetStockObject(WHITE_BRUSH));
-
+	/*
 	TRIVERTEX trives[3];
 	trives[0].x = bufRect.left;
 	trives[0].y = bufRect.top;
@@ -298,7 +323,7 @@ LRESULT WINAPI OnEraseBkgnd_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 	rects[1].UpperLeft = 1;
 	rects[1].LowerRight = 2;
 	GradientFill(hdc, trives, 3, rects, 2, GRADIENT_FILL_RECT_V);
-
+	*/
 	/*
 	//创建兼容DC
 	HDC bufHdc = NULL;
@@ -324,4 +349,133 @@ LRESULT WINAPI OnEraseBkgnd_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
 	DeleteDC(bufHdc);
 	*/
 	return  1;// DefWindowProc(hWnd, WM_ERASEBKGND, wParam, lParam);//返回非零表示擦除背景
+}
+
+
+LRESULT WINAPI OnCreate_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+
+	RECT wndRect;
+	if (!GetClientRect(hWnd, &wndRect))
+	{
+		return 0;
+	}
+
+	g_btnClose = CreateWindow(
+		TEXT("BUTTON"),
+		NULL,
+		BS_PUSHBUTTON | BS_OWNERDRAW | WS_CHILD | WS_VISIBLE,
+		wndRect.right - g_nTktBorder - 30, g_nTktBorder,
+		30, 20,
+		hWnd,
+		(HMENU)IDC_BTN_CLOSE,
+		g_hInstance,
+		NULL);
+	
+	return 0;
+}
+
+LRESULT WINAPI OnSize_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	RECT wndRect;
+	if (!GetClientRect(hWnd, &wndRect))
+	{
+		return 0;
+	}
+
+	MoveWindow(g_btnClose, wndRect.right - g_nTktBorder - 30, g_nTktBorder, 30, 20, TRUE);
+
+	return 0;
+}
+
+LRESULT WINAPI OnDrawItem_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	PDRAWITEMSTRUCT pDrawSt = (PDRAWITEMSTRUCT)lParam;
+
+	switch (pDrawSt->CtlID)
+	{
+	case IDC_BTN_CLOSE:
+	{
+
+		//用于按钮绘制的DC
+		HDC dc = pDrawSt->hDC;
+
+		//判断按钮各种状态的BOOL值
+		BOOL bIsPressed = (pDrawSt->itemState & ODS_SELECTED);
+		BOOL bIsFocused = (pDrawSt->itemState & ODS_FOCUS);
+		BOOL bIsDisabled = (pDrawSt->itemState & ODS_DISABLED);
+		BOOL bDrawFocusRect = !(pDrawSt->itemState & ODS_NOFOCUSRECT);
+		RECT itemRect = pDrawSt->rcItem; //按钮的矩形区域
+
+	}
+		return TRUE;
+	}
+
+	return DefWindowProc(hWnd, WM_DRAWITEM, wParam, lParam);
+}
+
+
+LRESULT WINAPI OnMoveMouse_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	POINTS point = { 0 };
+	point = MAKEPOINTS(lParam);
+
+	RECT wndRect;
+	if (!GetClientRect(hWnd, &wndRect))
+	{
+		return 0;
+	}
+
+
+	int width = wndRect.right - wndRect.left;
+	int height = wndRect.bottom - wndRect.top;
+
+	//设置光标显示
+	if (//标题栏
+		point.x > g_nTktBorder && point.x < (width - g_nTktBorder) &&
+		point.y > g_nTktBorder && point.y <= g_nCapBorder + g_nTktBorder
+		)
+	{
+		SetCursor(LoadCursor(NULL, IDC_SIZEALL));
+	}
+	else if (//在窗口水平边框的上方或底部
+		point.x > g_nTktBorder && point.x < (width - g_nTktBorder) &&
+		(point.y >= 0 && point.y <= g_nTktBorder || 
+		point.y >= (height - g_nTktBorder) && point.y <= height)
+		)
+	{
+		SetCursor(LoadCursor(NULL, IDC_SIZENS));
+	}
+	else if (//在窗口的左边框上或右边框上
+		(point.x >= (width - g_nTktBorder) && point.x <= width || 
+		point.x >= 0 && point.x <= g_nTktBorder) &&
+		point.y > g_nTktBorder && point.y < (height - g_nTktBorder)
+		)
+	{
+		SetCursor(LoadCursor(NULL, IDC_SIZEWE));
+	}
+	else if (//在窗口边框的左上角或右下角
+		(point.x >= 0 && point.x <= g_nTktBorder &&
+		point.y >= 0 && point.y <= g_nTktBorder )||
+		(point.x >= (width - g_nTktBorder) && point.x <= width &&
+		point.y >= (height - g_nTktBorder) && point.y <= height)
+		)
+	{
+		SetCursor(LoadCursor(NULL, IDC_SIZENWSE));
+	}
+	else if (//在窗口边框的左下角或右上角
+		(point.x >= 0 && point.x <= g_nTktBorder &&
+		point.y >= (height - g_nTktBorder) && point.y <= height )||
+		(point.x >= (width - g_nTktBorder) && point.x <= width &&
+		point.y >= 0 && point.y <= g_nTktBorder)
+		)
+	{
+		SetCursor(LoadCursor(NULL, IDC_SIZENESW));
+	}
+	else
+	{
+		SetCursor(LoadCursor(NULL, IDC_ARROW));
+	}
+
+	return 0;
 }
--- MyProject/main.h
+++ MyProject/main.h
@@ -18,13 +18,20 @@ LRESULT CALLBACK WinMainProc(
 bool InitApplication(
 	HINSTANCE hInstance);
 
+bool InitControl(
+	HWND hWnd);
+
 //MainWnd Message
 LRESULT WINAPI OnClose_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI OnCreate_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI OnDestroy_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI OnEraseBkgnd_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI OnGetMinMaxInfo_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI OnLbuttonDown_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI OnPaint_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI OnSize_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI OnDrawItem_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI OnMoveMouse_MainWnd(HWND hWnd, WPARAM wParam, LPARAM lParam);
 
 
 # endif
